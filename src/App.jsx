3430b84f6f12af1cd9a8696b50272f457831527d
Aishi Ashirvad Mishra <aishiashirvad@gmail.com>
2026-05-11 21:42:07 +0530

    React router, multi page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState, useEffect } from 'react'
 import './App.css'
 import GitHubCard from './components/GitHubCard'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <GitHubCard />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/github" element={<GitHubCard />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
