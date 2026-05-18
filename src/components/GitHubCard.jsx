bdcf50f9cfb1c5692f1188edabeba8aeae1d86cf
Aishi Ashirvad Mishra <aishiashirvad@gmail.com>
2026-05-18 17:14:52 +0530

    typo fixes and github card updates

--- src/components/GitHubCard.jsx
+++ src/components/GitHubCard.jsx
@@ -10,42 +10,60 @@ function GitHubCard() {
     // const [userName, setUserName] = useState(username || 'Akanha17');
     const [searchInput, setSearchInput] = useState('');
     const { userName , setUserName} = useContext(UserContext);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         if(username){
             setUserName(username);
+            setUserData(null);
+            setError(null);
         }
+    }, [username])
+    useEffect(() => {
+        if(!userName) return;
         // fetch('https://api.github.com/users/octocat')
         //     .then(response => response.json())
         //     .then(data => setUserData(data))
         //     .catch(error => console.error('Error fetching data:', error));
         async function fetchData() {
+            setLoading(true);
+            setError(null);
             try {
                 const response = await fetch(`https://api.github.com/users/${userName}`, {
                     headers: {
-                        'authorization': `token ${import.meta.env.VITE_GITHHUB_TOKEN}`
+                        'authorization': `token ${import.meta.env.VITE_GITHUB_TOKEN}`
                     }
                 });
                 const data = await response.json();
-                setUserData(data);
+                if(data.message === "Not Found"){
+                    setError('User not found 🧐');
+                    setUserData(null);
+                    return;
+                }else{
+                    setUserData(data);
+                }
             }
             catch (error) {
+                setError('Something went wrong 😑')
                 console.error('Error fetching data:', error);
+            }
+            finally{
+                setLoading(false);
             }
 
         }
+
         fetchData();
     }, [userName]);
-
-    if (!userData) {
-        return <div>Loading...</div>;
-    }
-
-    return (
+    if(loading) return <div className='status'>Loading...</div>;
+    if(error) return <div className='status error'>{error}</div>;
+    if(!userData) return null
+    else return (
         <div className='github-card-container'>
             <div className="input">
                 <input type='text' className="input-field" placeholder='Enter GitHub username' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
-                <button class="button" onClick={() => setUserName(searchInput)}>Search</button>
+                <button className="button" onClick={() => setUserName(searchInput)}>Search</button>
             </div>
             <div className="github-card">
                 <img src={userData.avatar_url} alt={`${userData.login}'s avatar`} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,20 @@
 import { useNavigate } from "react-router-dom"
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { UserContext } from "../context/UserContext"
 
 function Home(){
     const [input, setInput] = useState('')
     const navigate = useNavigate();
-    function haldleSearch(username){
-        navigate(`/github/${username}`);
+    const {setUserName} = useContext(UserContext);
+    function handleSearch(){
+        setUserName('');
+        navigate(`/github/${input}`);
     }
     return (
         <>
             <h1>Github Tracker</h1>
             <input type="text" placeholder="Enter GitHub username" value={input} onChange={(e) => setInput(e.target.value)} />
-            <button onClick={() => haldleSearch(input)}>Search</button>
+            <button onClick={() => handleSearch(input)}>Search</button>
         </>
     )
 }
